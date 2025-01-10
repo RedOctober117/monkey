a92b87352939188fc53428c19b6d387f1b2a3ac6
telemakos <dbcahilly3@gmail.com>
2025-01-10 12:54:39 -0600

    working state machine interpreter

--- src/root.zig
+++ src/root.zig
@@ -1,26 +1,49 @@
 const std = @import("std");
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
+const EnumMap = std.enums.EnumMap;
 const fields = std.meta.fields;
 
-const TokenTypeTag = enum { illegal, eof, ident, int, assign, plus, comma, semicolon, lparen, rparen, lbrace, rbrace, function, let };
+const TokenTypeTag = enum { illegal, eof, int, assign, plus, comma, semicolon, lparen, rparen, lbrace, rbrace, function, let, expression };
 
 const TokenType = union(TokenTypeTag) {
     illegal: u8,
     eof: void,
-    ident: []const u8,
     int: []const u8,
-    assign: u8,
-    plus: u8,
-    comma: u8,
-    semicolon: u8,
-    lparen: u8,
-    rparen: u8,
-    lbrace: u8,
-    rbrace: u8,
+    assign: void,
+    plus: void,
+    comma: void,
+    semicolon: void,
+    lparen: void,
+    rparen: void,
+    lbrace: void,
+    rbrace: void,
     function: void,
     let: void,
+    expression: []const u8,
 };
+
+const Operator = enum {
+    assign,
+    plus,
+    comma,
+    semicolon,
+    lparen,
+    rparen,
+    lbrace,
+    rbrace,
+};
+
+const OperatorMap = EnumMap(Operator, u8).init(.{
+    .assign = "=",
+    .plus = "+",
+    .comma = ",",
+    .semicolon = ";",
+    .lparen = "(",
+    .rparen = ")",
+    .lbrace = "{",
+    .rbrace = "}",
+});
 
 const Token = struct {
     token_type: TokenType,
@@ -31,22 +54,20 @@ pub const Lexer = struct {
     const Self = @This();
 
     const State = enum {
-        init,
-        word,
-        number,
-        symbol,
+        expression,
+        operator,
+        whitespace,
+        illegal,
     };
 
     state: State,
-    read_position: u8, // current char being read
     input: []const u8,
     output: ArrayList(Token),
     allocator: Allocator,
 
     pub fn init(input: []const u8, alloc: Allocator) Self {
         return .{
-            .state = State.init,
-            .read_position = 0,
+            .state = State.illegal,
             .input = input,
             .output = ArrayList(Token).init(alloc),
             .allocator = alloc,
@@ -57,171 +78,75 @@ pub const Lexer = struct {
         self.output.deinit();
     }
 
-    fn has_next(self: Self) bool {
-        return self.read_position < self.input.len;
-    }
-
-    fn next(self: *Self) u8 {
-        const read_char = self.input[self.read_position];
-        self.read_position += 1;
-        return read_char;
-    }
-
-    fn advance(self: *Self) void {
-        self.read_position += 1;
-    }
-
-    fn peak(self: Self) ?u8 {
-        if (self.has_next()) {
-            return self.input[self.read_position + 1];
+    fn match_expression(input: []const u8) TokenType {
+        if (std.mem.eql(u8, input, "function")) {
+            return TokenType.function;
+        } else if (std.mem.eql(u8, input, "let")) {
+            return TokenType.let;
         } else {
-            return null;
+            return TokenType{ .expression = input };
         }
     }
 
-    fn parse_number_literal(self: *Self) Token {
-        const start_idx: u8 = self.read_position;
-        while (self.has_next()) {
-            switch (self.next()) {
-                '0'...'9' => {
-                    self.advance();
-                },
-                else => {
-                    self.advance();
-                    break;
-                },
-            }
+    fn change_state(self: *Self, input: u8) void {
+        self.state = switch (input) {
+            'A'...'Z',
+            'a'...'z',
+            '_',
+            '0'...'9',
+            => State.expression,
+            ' ', '\t', '\n', '\r' => State.whitespace,
+            '=', '+', ',', ';', '(', ')', '{', '}' => State.operator,
+            else => State.illegal,
+        };
+    }
+
+    fn match_operator(input: u8) TokenType {
+        return switch (input) {
+            '=' => TokenType.assign,
+            '+' => TokenType.plus,
+            ',' => TokenType.comma,
+            ';' => TokenType.semicolon,
+            '(' => TokenType.lparen,
+            ')' => TokenType.rparen,
+            '{' => TokenType.lbrace,
+            '}' => TokenType.rbrace,
+            else => TokenType{ .illegal = input },
+        };
+    }
+
+    fn parse_buffer(self: *Self, word: []const u8, index: u8) !void {
+        if (word.len > 0) {
+            const expression_type = match_expression(word);
+            try self.output.append(.{
+                .token_type = expression_type,
+                .position = @intCast(index - word.len),
+            });
         }
-
-        return .{
-            .token_type = TokenType{ .int = self.input[start_idx .. self.read_position - 1] },
-            .position = start_idx,
-        };
-    }
-
-    fn parse_string_literal(self: *Self) Token {
-        const start_idx: u8 = self.read_position;
-        while (self.has_next()) {
-            switch (self.next()) {
-                'A'...'Z', 'a'...'z' => {
-                    continue;
-                },
-                else => break,
-            }
-        }
-
-        var token = TokenType{ .ident = self.input[start_idx..self.read_position] };
-
-        if (std.mem.eql(u8, token.ident, "function")) {
-            token = TokenType.function;
-        } else if (std.mem.eql(u8, token.ident, "let")) {
-            token = TokenType.let;
-        }
-
-        return .{
-            .token_type = token,
-            .position = start_idx,
-        };
-    }
-
-    fn is_ascii_letter(token: u8) bool {
-        return switch (token) {
-            'A'...'Z', 'a'...'z' => true,
-            else => false,
-        };
-    }
-
-    fn is_ascii_number(token: u8) bool {
-        return switch (token) {
-            '0'...'9' => true,
-            else => false,
-        };
-    }
-
-    fn is_ascii_whitespace(token: u8) bool {
-        return switch (token) {
-            ' ', '\t', '\n', '\r' => true,
-            else => false,
-        };
     }
 
     pub fn tokenize(self: *Self) ![]Token {
-        for (self.input) |current_char| {
-            switch (self.state) {
-                State.init => {
-                    if (is_ascii_letter(current_char)) {
-                        self.state = State.word;
-                    }
+        var buffer: ArrayList(u8) = ArrayList(u8).init(self.allocator);
+        defer buffer.deinit();
+
+        for (self.input, 0..self.input.len) |current_char, index| {
+            const cast_index: u8 = @intCast(index);
+            self.change_state(current_char);
+            try switch (self.state) {
+                State.whitespace => {
+                    try self.parse_buffer(try buffer.toOwnedSlice(), cast_index);
                 },
-                State.word => {
-                    if (is_ascii_letter(current_char)) {
-                        self.state = State.word;
-                    }
+                State.expression => buffer.append(current_char),
+                State.illegal => try self.output.append(.{ .token_type = TokenType{ .illegal = current_char }, .position = cast_index }),
+                State.operator => {
+                    try self.parse_buffer(try buffer.toOwnedSlice(), cast_index);
+                    try self.output.append(.{ .token_type = match_operator(current_char), .position = @intCast(cast_index) });
                 },
-            }
-            switch (current_char) {
-                '=' => {
-                    try self.output.append(.{
-                        .token_type = TokenType{ .assign = '=' },
-                        .position = self.read_position,
-                    });
-                },
-                '+' => {
-                    try self.output.append(.{
-                        .token_type = TokenType{ .plus = '+' },
-                        .position = self.read_position,
-                    });
-                },
-                ',' => {
-                    try self.output.append(.{
-                        .token_type = TokenType{ .comma = ',' },
-                        .position = self.read_position,
-                    });
-                },
-                ';' => {
-                    try self.output.append(.{
-                        .token_type = TokenType{ .semicolon = ';' },
-                        .position = self.read_position,
-                    });
-                },
-                '(' => {
-                    try self.output.append(.{
-                        .token_type = TokenType{ .lparen = '(' },
-                        .position = self.read_position,
-                    });
-                },
-                ')' => {
-                    try self.output.append(.{
-                        .token_type = TokenType{ .rparen = ')' },
-                        .position = self.read_position,
-                    });
-                },
-                '{' => {
-                    try self.output.append(.{
-                        .token_type = TokenType{ .lbrace = '{' },
-                        .position = self.read_position,
-                    });
-                },
-                '}' => {
-                    try self.output.append(.{
-                        .token_type = TokenType{ .rbrace = '}' },
-                        .position = self.read_position,
-                    });
-                },
-                'A'...'Z', 'a'...'z' => try self.output.append(self.parse_string_literal()),
-                '0'...'9' => try self.output.append(self.parse_number_literal()),
-                ' ', '\t', '\n', '\r' => continue,
-                else => {
-                    try self.output.append(.{
-                        .token_type = TokenType{ .illegal = self.input[self.read_position] },
-                        .position = self.read_position,
-                    });
-                },
-            }
+            };
         }
         try self.output.append(.{
             .token_type = TokenType.eof,
-            .position = self.read_position,
+            .position = @intCast(self.input.len),
         });
         return self.output.toOwnedSlice();
     }
